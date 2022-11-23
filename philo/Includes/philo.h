d3168794f7d3537db1c6a7646f8fbe9bc866268b
Quilfort Frederiks <qfrederi@f0r2s17.codam.nl>
2022-11-23 08:59:24 +0100

    added atoi for arguments

--- philo/Includes/philo.h
+++ philo/Includes/philo.h
@@ -6,14 +6,28 @@
 /*   By: qfrederi <qfrederi@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/22 08:53:04 by qfrederi      #+#    #+#                 */
-/*   Updated: 2022/11/22 11:06:46 by qfrederi      ########   odam.nl         */
+/*   Updated: 2022/11/23 08:56:15 by qfrederi      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
+
+# include <stdbool.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <limits.h>
+# include <sys/time.h>
+
+typedef int				t_int32;
+typedef unsigned int	t_uint32;
+typedef long			t_int64;
+typedef unsigned long	t_uint64;
+
 # define PHILO_H
 
-# include <stdio.h>
-# include <stdbool.h>
+	// philo_utils
+int		ft_atoi(const char *str);
 
 #endif 
---
+++ philo/Src/philo_utils.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   philo_utils.c                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: qfrederi <qfrederi@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/11/23 08:51:42 by qfrederi      #+#    #+#                 */
+/*   Updated: 2022/11/23 08:55:16 by qfrederi      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	ft_atoi(const char *str)
+{
+	int	negative;
+	int	i;
+	int	num;
+
+	i = 0;
+	negative = 1;
+	num = 0;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\v'
+		|| str[i] == '\f' || str[i] == '\r')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			negative *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = num * 10 + (str[i] - '0');
+		i++;
+	}
+	return (num * negative);
+}
