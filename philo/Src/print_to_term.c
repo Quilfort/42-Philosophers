3c6682661eaf2bff07bec7066c21782caccb7d1b
Quilfort Frederiks <qfrederi@f1r6s10.codam.nl>
2022-11-30 11:55:32 +0100

    Went Through LazyPhilo without fail

--- philo/Src/print_to_term.c
+++ philo/Src/print_to_term.c
@@ -6,11 +6,23 @@
 /*   By: qfrederi <qfrederi@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/24 10:17:05 by qfrederi      #+#    #+#                 */
-/*   Updated: 2022/11/30 10:21:20 by qfrederi      ########   odam.nl         */
+/*   Updated: 2022/11/30 11:05:14 by qfrederi      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while ((s1[i] != '\0') && (s1[i] == s2[i]) && (i + 1 < n))
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
 
 int	error_message(t_vars *vars, char *message)
 {
@@ -24,7 +36,7 @@ int	error_message(t_vars *vars, char *message)
 
 void	print_message(t_vars *vars, t_philo *philo, char *string)
 {
-	if (vars->active == true)
+	if (vars->active == true || ft_strncmp(string, "died", 4) == 0)
 	{
 		pthread_mutex_lock(&vars->lock_print);
 		printf("%lu %d %s\n", time_of_day(), philo->id, string);
